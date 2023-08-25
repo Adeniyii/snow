0143bdbaba66d69fab12977b53445452249cc951
Adeniyii <ifedayoadeniyi@gmail.com>
2023-08-25 21:16:22 +0100

    feat: define initial token type constants
    This is not an exhaustive list, and will be expanded upon as we continue.

--- token/token.go
+++ token/token.go
@@ -8,3 +8,29 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+const (
+	ILLEGAL = "ILLEGAL" // identifies unknown characters
+	EOF     = "EOF"     // signifies the end of file
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+	MINUS  = "-"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	// special characters
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
