1748637cef85f659d09a8fbbe05b041cf885ddb8
Adeniyii <ifedayoadeniyi@gmail.com>
2023-09-01 22:20:47 +0100

    feat: define ast types

---
+++ ast/ast.go
@@ -0,0 +1,40 @@
+package ast
+
+import "snow/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token *token.Token
+	Name  Expression
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
