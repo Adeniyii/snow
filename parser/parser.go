8463eced19c02669b3e64d9e01878b3b00305551
Adeniyii <ifedayoadeniyi@gmail.com>
2023-09-14 23:27:08 +0100

    feat: implement basic identifier parsing
    Not exhaustive, and will be improved upon shortly.

--- parser/parser.go
+++ parser/parser.go
@@ -11,6 +11,23 @@ import (
 	"snow/token"
 )
 
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
+
+// precedence order assignment
+const (
+	_ int = iota
+	LOWEST
+	EQS    // ==
+	LLGG   // > or <
+	SUM    // +
+	PROD   // *
+	PREFIX // !X or -X
+	CALL   // fn()
+)
+
 // The parser structure holds the parser's internal state.
 // currToken holds the token currently being parsed,
 // nextToken holds the next token to be parsed.
@@ -19,23 +36,33 @@ type Parser struct {
 	currToken token.Token
 	nextToken token.Token
 	errors    []string
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l}
 	p.errors = []string{}
+
 	p.readToken()
 	p.readToken()
+
+	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
+
 	return p
 }
 
-// readToken advances the currToken and nextToken fields of the Parser
-// using the NextToken method of the Lexer.
-func (p *Parser) readToken() {
-	p.currToken = p.nextToken
-	p.nextToken = p.l.NextToken()
+// parseIdentifier is a parsing function for the IDENT token type.
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: p.currToken}
 }
 
+// ParseProgram sets up the program structures and kicks off parsing of the available tokens.
+// It populates the statements slice with the statement node returned from each parsing operation.
+// It does this until it encounteres an EOF token indicating the end of the program.
+// Any errors encountered during parsing will be stored in the errors slice.
 func (p *Parser) ParseProgram() *ast.Program {
 	program := &ast.Program{}
 	program.Statements = []ast.Statement{}
@@ -50,6 +77,8 @@ func (p *Parser) ParseProgram() *ast.Program {
 	return program
 }
 
+// parseStatement is a small engine that decides which parsing function to
+// should handle parsing of the current statement/expression.
 func (p *Parser) parseStatement() ast.Statement {
 	switch p.currToken.Type {
 	case token.LET:
@@ -57,10 +86,13 @@ func (p *Parser) parseStatement() ast.Statement {
 	case token.RETURN:
 		return p.parseReturnStatement()
 	default:
-		return nil
+		return p.parseExpressionStatement()
 	}
 }
 
+// parseLetStatement handles parsing of let statements.
+// if the statement is invalid, an error is registered on the parser state
+// and the function returns nil, otherwise it returns a LetStatement node.
 func (p *Parser) parseLetStatement() *ast.LetStatement {
 	stmt := &ast.LetStatement{Token: p.currToken}
 
@@ -81,16 +113,29 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	return stmt
 }
 
+// parseReturnStatement handles parsing of return statements.
+// if the statement is invalid, an error is registered on the parser state
+// and the function returns nil, otherwise it returns a ReturnStatement node.
 func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	stmt := &ast.ReturnStatement{Token: p.currToken}
 
 	p.readToken()
 
-	for !p.currTokenIs(token.SEMICOLON) {
+	for !p.currTokenIs(token.SEMICOLON) { // skipping over the expressions for now
 		p.readToken()
 	}
 
 	return stmt
+}
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmnt := &ast.ExpressionStatement{Token: p.currToken}
+	stmnt.Expression = p.parseExpression(LOWEST)
+
+	if p.nextTokenIs(token.SEMICOLON) { // semicolons are optional
+		p.readToken()
+	}
+	return stmnt
 }
 
 func (p *Parser) expectPeek(t token.TokenType) bool {
@@ -100,6 +145,13 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 	}
 	p.pushError(t)
 	return false
+}
+
+// readToken advances the currToken and nextToken fields of the Parser
+// using the NextToken method of the Lexer.
+func (p *Parser) readToken() {
+	p.currToken = p.nextToken
+	p.nextToken = p.l.NextToken()
 }
 
 func (p *Parser) nextTokenIs(t token.TokenType) bool {
@@ -117,4 +169,21 @@ func (p *Parser) Errors() []string {
 func (p *Parser) pushError(t token.TokenType) {
 	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.nextToken.Type)
 	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) registerPrefix(t token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[t] = fn
+}
+
+func (p *Parser) registerInfix(t token.TokenType, fn infixParseFn) {
+	p.infixParseFns[t] = fn
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	prefix := p.prefixParseFns[p.currToken.Type]
+	if prefix == nil {
+		return nil
+	}
+	leftexp := prefix()
+	return leftexp
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -74,6 +74,38 @@ return 6 * 9;
 	}
 }
 
+func TestIdentifierExpression(t *testing.T) {
+	input := "foobar;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has invalid statement count. got=%d",
+			len(program.Statements))
+	}
+
+	stmnt, ok := program.Statements[0].(*ast.ExpressionStatement)
+
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+
+	ident, ok := stmnt.Expression.(*ast.Identifier)
+
+	if !ok {
+		t.Fatalf("exp not *ast.Identifier. got=%T", stmnt.Expression)
+	}
+	if ident.TokenLiteral() != "foobar" {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", "foobar",
+			ident.TokenLiteral())
+	}
+}
+
 func testLetStatement(t *testing.T, stmnt ast.Statement, name string) bool {
 	if stmnt.TokenLiteral() != "let" {
 		t.Fatalf("stmnt.TokenLiteral() not 'let'. got %q", stmnt.TokenLiteral())
